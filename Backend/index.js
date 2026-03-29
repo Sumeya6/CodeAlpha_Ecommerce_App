b946022f779a6da0d9544013cf1f58b8f99f942d
Sumeya6 <sumeyabahiru55@gmail.com>
2026-03-29 09:21:58 -0700

    fix: update CORS configuration to allow requests from localhost

--- Backend/index.js
+++ Backend/index.js
@@ -12,12 +12,11 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-    {
-        origin:"true",
-        credentials:"true"
-    }
-));
+
+app.use(cors({
+  origin: "http://localhost:5173", 
+  credentials: true
+}));
 
 //routes
 app.use("/api/auth",authRoutes);
